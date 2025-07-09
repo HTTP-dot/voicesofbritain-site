b0cfcd05d31a9ad72a06ad3bafb8a9e803d18d3c
HTTP-dot <76542877+HTTP-dot@users.noreply.github.com>
2025-07-09 23:50:01 +0000

    Fix: corrected layout folder config

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,10 @@
 const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
-  // Add date filter
   eleventyConfig.addFilter("date", (dateObj, format = "dd LLL yyyy") => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(format);
   });
 
-  // Add post collection
   eleventyConfig.addCollection("post", function (collectionApi) {
     return collectionApi.getFilteredByGlob("./posts/*.md");
   });
